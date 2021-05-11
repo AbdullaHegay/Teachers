c9ae51d6e3b6d0ff002b42a3f21816fbcde5e553
AbdullaHegay <74024801+AbdullaHegay@users.noreply.github.com>
2021-05-11 23:40:40 +0600

    Teachers
    Alatoo uni teachers base

--- src/main/java/com/example/demo/repository/MainRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MainRepository extends JpaRepository<Main, Long> {}
